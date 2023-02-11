e55990dd462e0cf8d66e313980b49164d53736f1
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-11 10:32:17 -0800

    Plantilla para comenzar a trabajar

---
+++ P1_UART/main/main.c
@@ -0,0 +1,18 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "driver/uart.h"
+#include "esp_log.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+
+#define MASTER      0
+#define SLAVE    1
+#define MODE        SLAVE  
+
+void app_main(void)
+{
+
+}

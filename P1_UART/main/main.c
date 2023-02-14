b985cb150220c2f16bfa734741d4d18e8367e427
Daniel Santos <santos.daniel88@uabc.edu.mx>
2023-02-14 14:58:46 -0800

    Modifique epara los cmds requeridos

--- P1_UART/main/main.c
+++ P1_UART/main/main.c
@@ -20,26 +20,42 @@
 
 void app_main()
 {
+    uint8_t led_state = 0;
     gpio_reset_pin(LED);
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
     uartInit1();
     uartInit(0, BAUD_RATE, 8, eParityDis, eStop, UART_TX_PIN0, UART_RX_PIN0); // uart_num, baudrate,  size,  parity, stop,  txPin,  rxPin)
 
-    uint8_t rx_buffer[3];
+    uint8_t command[3];
     while (1)
     {
-        int len = uart_read_bytes(UART_NUM_1, rx_buffer, 3, pdMS_TO_TICKS(100));
-        if (len == 2 && rx_buffer[0] == 'O' && rx_buffer[1] == 'N')
+        int len = uart_read_bytes(UART_NUM_1, command, 3, pdMS_TO_TICKS(100));
+        if (len == 2 && command[0] == '1' && command[1] == '0')
         {
             gpio_set_level(LED, 1);
             uartClrScr(0);
-            uartPuts(0, "Prendido");
+            uartPuts(0, "Comando: 0x10");
         }
-        else if (len == 3 && rx_buffer[0] == 'O' && rx_buffer[1] == 'F' && rx_buffer[2] == 'F')
+        else if (len == 2 && command[0] == '1' && command[1] == '1')
         {
             gpio_set_level(LED, 0); // apaga el LED
             uartClrScr(0);
-            uartPuts(0, "Apagado");
+            uartPuts(0, "Comando: 0x11");
+        }
+        else if (len == 2 && command[0] == '1' && command[1] == '2')
+        {
+            uartClrScr(0);
+            uartPuts(0, "Comando: 0x12");
+        }
+        else if (len == 2 && command[0] == '1' && command[1] == '3')
+        {
+            uartClrScr(0);
+            uartPuts(0, "Comando: 0x13");
+        }
+        else
+        {
+            uartClrScr(0);
+            uartPuts(0, "Ese comando no existe");
         }
     }
 }
